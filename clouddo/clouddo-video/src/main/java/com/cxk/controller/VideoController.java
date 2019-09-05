89dfc03db57fe9b92056f72fcfcd84d4119b1569
hestia52 <1076232032@qq.com>
2019-09-05 16:11:26 +0800

    添加结构

--- clouddo/clouddo-video/src/main/java/com/cxk/controller/VideoController.java
+++ clouddo/clouddo-video/src/main/java/com/cxk/controller/VideoController.java
@@ -3,17 +3,16 @@ package com.cxk.controller;
 import com.cxk.pojo.Video;
 import com.cxk.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class VideoController {
     @Autowired
     private VideoService videoService;
-    /*@RequestMapping("/findAll")
-    @ResponseBody*/
+    @RequestMapping("/findAll")
     public List<Video> findAll() {
         return videoService.findAll();
     }
