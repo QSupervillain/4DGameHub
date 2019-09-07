891a32ac282b01461924e2fd4088997641abd49f
pengzhiqiang <demo18569451793@163.com>
2019-09-07 16:05:27 +0800

    templates

--- clouddo/clouddo-download/src/main/java/com/cxk/controller/IndexController.java
+++ clouddo/clouddo-download/src/main/java/com/cxk/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import sun.rmi.runtime.Log;
@@ -41,7 +42,7 @@ public class IndexController {
      */
 
     @RequestMapping("/load")
-    public String index() {
+    public String index(Model model) {
         long l1 = System.currentTimeMillis();
 
         List<GameType> gameTypeList = gameTypeService.getAll();
@@ -49,12 +50,14 @@ public class IndexController {
             System.out.println("gameType = " + gameType);
         }
 
-        List<DownLoad> downLoads = DownLoadPageHelper.downLoads(downLoadService, null, "download_time", "asc", 1, 3);
-        for (DownLoad downLoad : downLoads) {
+        List<DownLoad> downLoadList = DownLoadPageHelper.downLoads(downLoadService, null, "download_time", "asc", 1, 3);
+        for (DownLoad downLoad : downLoadList) {
             System.out.println("downLoad = " + downLoad);
         }
         long l2 = System.currentTimeMillis();
-        System.out.println("时间 = " + (l2 - l1));
+        model.addAttribute("gameTypeList", gameTypeList);
+        model.addAttribute("downLoadList", downLoadList);
+        System.out.println("load方法总耗时 = " + (l2 - l1));
         return "index4";
     }
 
